007b1e1a3a98c09d50c69e0fb73af418de431d6f
jsmithdataanalytics <jsmithdataanalytics@gmail.com>
2020-04-27 01:56:56 +0100

    Improved build process with error codes

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ An Airflow pipeline that regularly scrapes asking prices from Zoopla and perform
 3. ```source ./build.sh```
 
 ## Run locally
-1. In one tmux session: ```airflow webserver -p 8080```
-2. In another: ```airflow scheduler```
+1. In one shell: ```source ./config.sh && airflow webserver -p 8080```
+2. In another: ```source ./config.sh && airflow scheduler```
 3. In browser, go to Admin > Connections
 4. Create connection "sender_email" with host, port, login, password
-5. Turn on "house_price_tracker" DAG
+5. Under "extra" section, enter ```{"recipients": ["example1@gmail.com", "example2@hotmail.com"]}```
+6. Turn on "house_price_tracker" DAG
--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 sudo apt update && sudo apt upgrade
 sudo apt install curl
 sudo curl -fsSL -o /usr/local/bin/dbmate https://github.com/amacneil/dbmate/releases/download/v1.7.0/dbmate-linux-amd64
-sudo chmod +x /usr/local/bin/dbmate
-sudo cp ./example.config.sh ./config.sh
+sudo chmod +x /usr/local/bin/dbmate || exit 1
+sudo cp ./example.config.sh ./config.sh || exit 1
 sudo chmod 777 ./config.sh
-echo export AIRFLOW__CORE__FERNET_KEY=`python3 generate_fernet_key.py` | sudo tee -a ./config.sh
-source ./config.sh
-dbmate up
-airflow initdb
+echo export AIRFLOW__CORE__FERNET_KEY=`python3 generate_fernet_key.py` | sudo tee -a ./config.sh || exit 1
+source ./config.sh || exit 1
+dbmate up || exit 1
+airflow initdb || exit 1
+echo "Success!"
