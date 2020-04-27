b81c4ac20ebd3f61384fdb42d12bd8faac256b10
jsmithdataanalytics <jsmithdataanalytics@gmail.com>
2020-04-27 01:09:00 +0100

    Implemented send_email

--- dags/tasks/send_email.py
+++ dags/tasks/send_email.py
@@ -1,14 +1,50 @@
 import pandas as pd
 from utils import db
 from airflow.hooks.base_hook import BaseHook
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import date
 
 email_connection = BaseHook.get_connection('sender_email')
 
 
 def send_email():
+    table = analyse_prices()
+    __send_email(body=str(table))
+
+
+def __send_email(body: str):
+
+    if not date.today().weekday():
+        s = smtplib.SMTP(host=email_connection.host, port=email_connection.port)
+        s.starttls()
+        s.login(user=email_connection.login, password=email_connection.password)
+
+        for recipient in email_connection.extra_dejson['recipients']:
+            msg = MIMEMultipart()
+            msg['From'] = email_connection.login
+            msg['To'] = recipient
+            msg['Subject'] = 'Weekly House Price Report'
+            msg.attach(MIMEText(body, 'plain'))
+
+            s.send_message(msg)
+
+        s.quit()
+
+
+def analyse_prices():
     listings = pd.DataFrame(db.select_all('Listings'))
     listings.drop('id', axis=1, inplace=True)
     listings.set_index('listing_id', inplace=True)
     listings = listings.astype({'num_bedrooms': 'float64', 'listed_on': 'datetime64', 'last_reduced_on': 'datetime64'})
+    listings['date'] = listings.last_reduced_on.combine_first(listings.listed_on)
+    listings['week'] = listings.date.apply(lambda d: d.week)
+    listings['year'] = listings.date.apply(lambda d: d.year)
+    grouped = listings.groupby(['year', 'week'])
+    grouped.mean().join(grouped.count(), rsuffix='_count')
+    prices_by_week = grouped.mean().join(grouped.count(), rsuffix='_count')
+    prices_by_week_2020 = prices_by_week.loc[2020][['listing_price', 'listing_price_count']]
+    prices_by_week_2020.rename({'listing_price_count': 'count'}, axis=1, inplace=True)
 
-    return listings
+    return prices_by_week_2020
